0068c8ffaeddb827948e437667a48ca25b27c20b
Adewumi Adeyemo <adexadey2@gmail.com>
2020-09-16 14:50:23 +0100

    first task completed

---
+++ index.js
@@ -0,0 +1,63 @@
+// DOM EXERCISE
+/*Write the code necessary to do the following:
+	1. Select the section with an id of container without using querySelector .
+	2. Select the section with an id of container using querySelector .
+	3. Select all of the list items with a class of "second".
+	4. Select a list item with a class of third, but only the list item inside of the ol tag.
+	5. Give the section with an id of container the text "Hello!".
+	6. Add the class main to the div with a class of footer .
+	7. Remove the class main on the div with a class of footer .
+	8. Create a new li element.
+	9. Give the li the text "four".
+	10.Append the li to the ul element.
+	11.Loop over all of the li s inside the ol tag and give them a background color of
+	"green".
+	12.Remove the div with a class of footer.
+*/
+/*
+//#1
+let container = document.getElementById("container");
+//#2. 
+let containerQuerySelector = document.querySelector("#container");
+//#3.
+let secondLi = document.getElementsByClassName("second");
+//#4.*/
+let thirdLi = document.getElementByTagName
+//#5.*/
+
+//#6.*/
+
+//#7.*/
+
+//#8.*/
+let newListItem = document.
+//#9.*/
+
+//#10.*/
+
+//#11.*/
+
+//#12.*/
+
+/*
+EVENTS
+
+Part 1
+1. Add the necessary code to wait for the DOM to load to make sure that
+anything you manipulate in the DOM has loaded. You can do this either using
+window.onload or adding an event listener for DOMContentLoaded .
+2. Replace the text "Change me" with "Hello World!".
+3. When a user hovers over one of the colored boxes change the text to display
+the color that is being hovered over.
+4. Create a new div element.
+5. Give your new div a class of purple and style it so that it has a background
+color of purple.
+6. Append your new div to the page to the section tag.
+*/
+//#1. 
+window.addEventListener(DOMContentLoaded, ()=>{
+	a = document.getElementById("change_heading");
+	a.innerHTML("Hello World")
+})
+//#2. 
+
